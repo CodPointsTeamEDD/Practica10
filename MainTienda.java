4341911522bcb6fc514c3a53d0c7a8a26155df4f
Luis Fernando Quintana Lopez <fernandoquintana@ciencias.unam.mx>
2026-05-05 19:39:24 -0600

    deseenme suerte, voy a hacer el menu

--- MainTienda.java
+++ MainTienda.java
@@ -4,36 +4,38 @@ import java.io.IOException;
 
 public class MainTienda{
     public static void main(String[] args) {
-        /* Creando el inventario */
+        // Variables
         Inventario inv = new Inventario();
+		String nombreArchivo = "Productos.txt";
 
-        /* Nombre del archivo a leer */
-		String nombreArchivo = "participantes.txt";
-
-		/* Lectura del archivo para crear participates */
+		// Lectura del archivo para crear participates
         try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
-			/* Variable auxiliar que almacena la linea leida */
+			// Variables auxilires
             String linea;
+            int lineasLeidas = 0;
 
 
             while ((linea = br.readLine()) != null) {
-                String[] partes = linea.split(",");
+                if (lineasLeidas != 0) {
+                    String[] partes = linea.split(",");
 
-                String producto = partes[0];
-                double precio = Float.parseFloat(partes[1]);
-                String marca = partes[2];
-                String categoria = partes[3];
-                int existencia = Integer.parseInt(partes[4]);
+                    String producto = partes[0];
+                    double precio = Double.parseDouble(partes[1]);
+                    String marca = partes[2];
+                    String categoria = partes[3];
+                    int existencia = Integer.parseInt(partes[4]);
 
-                Producto porAgregar = new Producto(producto, precio, marca, categoria, existencia);
+                    Producto porAgregar = new Producto(producto, precio, marca, categoria, existencia);
 
-                inv.agregar(porAgregar);
-
+                    inv.agregar(porAgregar);
+                } else {
+                    lineasLeidas = lineasLeidas + 1;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(inv);
+
 
     }
 } 
